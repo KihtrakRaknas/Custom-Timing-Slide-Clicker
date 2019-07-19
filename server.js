e623d491cfad63c6d2a5dce88c1d97c03f638a81
Karthik Sankar <pi@kihtrak.com>
2019-07-18 20:42:07 -0400

    Enabled ext

--- server.js
+++ server.js
@@ -44,10 +44,10 @@ var fullScreenYet = false
       if(pi){
         broswerParams = {
             //args: ['--disable-infobars'],
-		/*args: [
+		args: [
 			`--disable-extensions-except=${CRX_PATH}`,
     			`--load-extension=${CRX_PATH}`
-		],*/
+		],
             headless: false,
             defaultViewport: null,
             executablePath: '/usr/bin/chromium-browser',
